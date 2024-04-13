9c9fd72eef6fe46471f22fcbfb668cf98134fdff
Otherwa <atharvdesai2002@gmail.com>
2024-04-13 11:42:18 +0530

    /Cookies/

---
+++ CookieDemo/CookieDemo/BookDetails.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookDetails.aspx.cs" Inherits="CookieDemo.BookDetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="SelBooks" runat="server" Width="500px"></asp:ListBox>
+            <br/>
+            <br/>
+            <asp:LinkButton ID="LinkButton1" runat="server" Font-Bold="True" PostBackUrl="~/Books.aspx">Select Books</asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CookieDemo/CookieDemo/BookDetails.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CookieDemo
+{
+    public partial class BookDetails : System.Web.UI.Page
+    {
+        HttpCookieCollection cookies;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cookies = new HttpCookieCollection();
+            cookies = Request.Cookies;
+            if (cookies.Count > 0)
+            {
+                foreach (string key in cookies.Keys)
+                {
+                    ListItem item = new ListItem() { Text = cookies[key].Value.ToString(), Value = key };
+                    SelBooks.Items.Add(item);
+                }
+            }
+        }
+
+    }
+}
