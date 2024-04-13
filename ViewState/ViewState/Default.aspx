5e51eacb00542d0b7822cae9eae6ca99f5c2c2d3
Otherwa <atharvdesai2002@gmail.com>
2024-04-13 09:46:11 +0530

    /ViewState ++

---
+++ ViewState/ViewState/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ViewState.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 100vh; padding: 1rem">
+
+
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="Name"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox runat="server" ID="Addr"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="BtnSubmit_Click" />
+            <asp:Button ID="btnRestore" runat="server" Text="Restore" OnClick="BtnRestore_Click" />
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ViewState/ViewState/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewState
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            ViewState["name"] = Name.Text.ToString();
+            ViewState["address"] = Addr.Text.ToString();
+            Name.Text = "";
+            Addr.Text = "";
+        }
+
+        protected void BtnRestore_Click(object sender, EventArgs e)
+        {
+            if (ViewState["name"] != null)
+            {
+                Name.Text = ViewState["name"].ToString();
+            }
+
+            if (ViewState["address"] != null)
+            {
+                Addr.Text = ViewState["address"].ToString();
+            }
+        }
+    }
+}
